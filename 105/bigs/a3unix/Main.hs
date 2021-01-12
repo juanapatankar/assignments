83efcd88258d13ece2c81ec649b3c77acbd7579a
Juana Patankar <juanapatankar4@gmail.com>
2021-01-12 15:48:32 +0000

    code accidentally works for maze 4. Compiles, works :) ish

--- 105/bigs/a3unix/Main.hs
+++ 105/bigs/a3unix/Main.hs
@@ -3,7 +3,7 @@ module Main (get_maze, print_maze, is_wall, place_player, move, can_move, game_l
 import System.Environment
 
 --maze_path = "assignments/105/bigs/a3unix/maze-big-4.txt"
-maze_path = "assignments/105/bigs/a3unix/maze2.txt"
+maze_path = "/newpartition/assignments/105/bigs/a3unix/maze3.txt"
 --maze_path = "assignments/105/bigs/a3unix/maze4.txt"
 
 -- Useful code from Lecture 25
@@ -83,10 +83,44 @@ game_loop m (x,y) = do
 ---- Part C
 
 -- Question 8
+
+try_up m start
+    | (can_move m start 'w' == True) = move start 'w'
+    | otherwise = (0,0)
+try_down m start
+    | (can_move m start 's' == True) = move start 's'
+    | otherwise = (0,0)
+try_right m start
+    | (can_move m start 'd' == True) = move start 'd'
+    | otherwise = (0,0)
+try_left m start 
+    | (can_move m start 'a' == True) = move start 'a'
+    | otherwise = (0,0)
+
+get_successors m start = let {up = try_up m start; down = try_down m start; left = try_left m start; right = try_right m start} in [down:(try_down m down):(try_left m down):(try_right m down):[], [right]]
+
+countFalse [] = 0
+countFalse (x:xs) = if x == False then (1+countFalse xs) else countFalse xs
+leaforbranch m test 
+
+    | (let {u = can_move m test 'w'; d = can_move m test 's'; l = can_move m test 'a'; r = can_move m  test 'd'} in countFalse [u,d,l,r] == 3) = "Leaf"
+    | otherwise = "Branch"
+
 -- move_me m (a,b) (x,y) 
    -- | is_wall m (a,b)  = "yes"
     -- | otherwise = "no"
+
+
+frontier m start 
+    | (can_move m start 's' ) = start:(frontier m (move start 's'))
+    | (can_move m start 'd') = start:(frontier m (move start 'd'))
+    | otherwise = [start]
+
+
 get_path :: [String] -> (Int, Int) -> (Int, Int) -> [(Int, Int)]
+get_path m start target = frontier m start 
+-- get_path maze_path current_position target_position
+--
 --get_path m start target = if start == target then [start] else [(21323213,2133213)]
 -- get_path m start target
    --  |( start == target) = [start]
@@ -94,11 +128,21 @@ get_path :: [String] -> (Int, Int) -> (Int, Int) -> [(Int, Int)]
 --get_path m (x,y) target = if (x,y) == target then [(x,y)] else if can_move m (x,y) 'd' then (x,y):get_path m (x+1,y) target 
 --else if can_move m (x,y) 's' then (x,y):get_path m (x, y+1) target else [(2,132321212)
 
-
 -- Question 9
+new_maze m [] = m
+new_maze m (x:xs) = new_maze (place_dot m x) xs
+
+place_dot m (x,y) = set m x y '.'
 
 main :: IO ()
-main = error "Not implemented"
+main = do
+    args <- getArgs
+    let path = head args
+    m <- get_maze path
+    --get_path m (1,1) 
+    print_maze (new_maze m (get_path  m (1,1) (7,7)))
+    
+   
 
 
 
