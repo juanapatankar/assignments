3921365b517e129b0e8b4d71dc02dfc406d2b271
Juana Patankar <juanapatankar4@gmail.com>
2021-02-27 18:39:32 +0000

    decodes properly :)

--- 122/caesar/Caesar.java
+++ 122/caesar/Caesar.java
@@ -1,20 +1,51 @@
-import java.util.Arrays;
 class Caesar {
 
     public static char rotate(int shift, char input) {
         String lowerletters = "abcdefghijklmnopqrstuvwxyz";
+        String upperletters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         if (lowerletters.indexOf(input) != -1) {
-            return lowerletters.charAt(lowerletters.indexOf(input) + shift);
+            if (shift >= 0) {
+                return lowerletters.charAt((lowerletters.indexOf(input) + shift) % 26);
+            } else {
+                shift = 26 + (shift % 26);
+                return lowerletters.charAt((lowerletters.indexOf(input) + shift) % 26);
+            }
+        } else if (upperletters.indexOf(input) != -1) {
+            if (shift >= 0) {
+                return upperletters.charAt((upperletters.indexOf(input) + shift) % 26);
+            } else {
+                shift = 26 + (shift % 26);
+                return upperletters.charAt((upperletters.indexOf(input) + shift) % 26);
+            }
         } else {
-            return 'n';
+            return input;
         }
     }
 
     public static String rotate(int shift, String input) {
-        return "hello";
+        String result = "";
+        for (int i=0; i < input.length(); i++ ) {
+            result += rotate(shift, input.charAt(i));
+
+        }
+        return result;
     }
 
+    public static void correctusage() {
+        System.out.println("Usage: java Caesar n [original text]");
+    }
     public static void main(String[] args) {
-        System.out.println(rotate(3, args[0].charAt(0)));
+        if (args.length == 2) {
+            System.out.println(rotate(Integer.parseInt(args[0]), args[1]));
+        } else if (args.length == 0) {
+            System.out.println("No arguments given!");
+            correctusage();
+        } else if (args.length == 1) {
+            System.out.println("You've missed an argument!");
+            correctusage();
+        } else {
+            System.out.println("Too many arguments");
+            correctusage();
+        }
     }
 }
