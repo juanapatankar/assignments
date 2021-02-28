1f43bef11559de9f5658ffcca1a03148b32096a7
Juana Patankar <juanapatankar4@gmail.com>
2021-02-28 19:25:37 +0000

    almost done with commenting :) made a new method, stripped, instead of repeating the letter check all the time

--- 122/caesar/Brutus.java
+++ 122/caesar/Brutus.java
@@ -1,26 +1,17 @@
 class Brutus {
+    /** Sets a double[] constant, containing the frequency of each english letter, in order. E.g. 'a' has a frequency of 0.0855, 'e' is the most common letter (frequency of 0.1210), 'z' has a frequency of 0.0011 */
     public static final double[] english = {
         0.0855, 0.0160, 0.0316, 0.0387, 0.1210, 0.0218, 0.0209, 0.0496, 0.0733, 0.0022, 0.0081, 0.0421, 0.0253, 0.0717, 0.0747, 0.0207, 0.0010, 0.0633, 0.0673, 0.0894, 0.0268, 0.0106, 0.0183, 0.0019, 0.0172, 0.0011
     };
-
-    public static int[] count(String input) {
-        input = input.toLowerCase();
-        int[] lettercount = new int[26];
-        String lowerletters = "abcdefghijklmnopqrstuvwxyz";
-        for (int i=0; i < input.length(); i++) {
-            char stringchar = input.charAt(i);
-            if (lowerletters.indexOf(stringchar) != -1) {
-                int letterindex = lowerletters.indexOf(stringchar);
-                lettercount[letterindex] += 1;
-            }
-        }
-        return lettercount;    
-    }
-
+    
+    /*** This method, strip, takes one input, a string. It returns a string that contains only the letter characters in the input, with everything else removed */
     public static String strip(String input) {
+        // Converts the input to lowercase, so that it's easier to compare with string lowerletters. 
         input = input.toLowerCase();
         String lowerletters = "abcdefghijklmnopqrstuvwxyz";
+        // Initialise strippedinput as an empty string
         String strippedinput = "";
+        // Iterates through the supplied input. Adds only characters that are in lowerletters to strippedinput, in the same order that they originally appear
         for (int i = 0; i < input.length(); i++) {
             char stringchar = input.charAt(i);
             if (lowerletters.indexOf(stringchar) != -1) {
@@ -30,18 +21,33 @@ class Brutus {
         return strippedinput;
     }
 
+    /*** The count method takes one input as a string, and returns an int[] lettercount. Lettercount is an array of 26 elements, one for each letter, consisting of the number of times a letter appears in the string (either upper or lowercase). */
+    public static int[] count(String input) {
+        // Converts the input to lowercase, as case doesn't need to be preserved for this method's purpose
+        // Initialise a blank array with length 26 (for 26 English letters). Create a string containing lowercase letters, in order, to easily add to the array in the correct position for comparing later
+        int[] lettercount = new int[26];
+        // Removes all non-letter characters from the input string
+        input = strip(input);
+        String lowerletters = "abcdefghijklmnopqrstuvwxyz";
+        // Iterate through each character in the input string. 
+        for (int i=0; i < input.length(); i++) {
+            char stringchar = input.charAt(i);
+            // If the character is a letter, it matches this with the correct position in lowerletters, so that the correct element in lettercount can be incremented. Otherwise, move onto the next character in the string
+            int letterindex = lowerletters.indexOf(stringchar);
+            lettercount[letterindex] += 1;
+        }
+        return lettercount; 
+    }   
+
+    /*** This method, frequency, takes one string as input, and returns an array double[], containing the frequency of each letter in the input. This is normalised by dividing by the total length of the stripped input */
     public static double[] frequency(String input) {
         double[] frequency = new double[26];
         String lowerletters = "abcdefghijklmnopqrstuvwxyz";
         input = strip(input);
-        
-
         for (int i=0; i < input.length(); i++) {
             char stringchar = input.charAt(i);
-            if (lowerletters.indexOf(stringchar) != -1) {
-                int letterindex = lowerletters.indexOf(stringchar);
-                frequency[letterindex] += 1;
-            }
+            int letterindex = lowerletters.indexOf(stringchar);
+            frequency[letterindex] += 1;
         }
         for (int i=0; i < 26; i++) {
             frequency[i] /= input.length();
