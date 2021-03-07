ae3dca39c157791be513bced1cb581bc84f9f698
Juana Patankar <juanapatankar4@gmail.com>
2021-03-07 17:50:27 +0000

    need to check logic on last sample

--- 108/Assignments/COMP108A1Paging.java
+++ 108/Assignments/COMP108A1Paging.java
@@ -92,17 +92,74 @@ class COMP108A1Paging {
 		return output;
 	}
 
+	private static int findLeastUsedIndex(int[] usageCount, int cSize) {
+		int leastUsedIndex = 0;
+		int leastUsedValue = usageCount[0];
+		for (int i = 1; i < cSize; i++) {
+			if (leastUsedValue > usageCount[i]) {
+				leastUsedValue = usageCount[i];
+				leastUsedIndex = i;
+			}
+		}
+		return leastUsedIndex;
+	}
+
 	// evict LFU
 	// Aim:
-	// evict the number that is least freently used so far if next request is not in cache
+	// evict the number that is least frequently used so far if next request is not in cache
 	// count number of hit and number of miss, and find the hit-miss pattern; return an object COMP108A1Output
 	// Input:
 	// cArray is an array containing the cache with cSize entries
-	// rArray is an array containing the requeset sequence with rSize entries
+	// rArray is an array containing the request sequence with rSize entries
 	static COMP108A1Output evictLFU(int[] cArray, int cSize, int[] rArray, int rSize) {
+		int indexToEvict = 0;
+		int[] usageCount = new int[cSize];
 		COMP108A1Output output = new COMP108A1Output();
-
+		for (int i = 0; i < rSize; i++) {
+			boolean foundReq = false;
+			for (int j = 0; j < cSize; j++) {
+				if (cArray[j] == rArray[i]) {
+					foundReq = true;
+					usageCount[j]++;
+				}
+			}
+			if (!foundReq) {
+				output.missCount++;
+				output.hitPattern += "m";
+				indexToEvict = findLeastUsedIndex(usageCount, cSize);
+				cArray[indexToEvict] = rArray[i];
+			} else {
+				output.hitCount++;
+				output.hitPattern += "h";
+			}
+		}
 		return output;
+	}
+
+	private static int[] generateDistances(int[] cArray, int cSize, int[] rArray, int rSize) {
+		int[] nextCall = new int[cSize];
+		for (int i = 0; i < cSize; i++) {
+			for (int j = 0; j < rSize; j++) {
+				nextCall[i]++;
+				if (rArray[j] == cArray[i]) {
+					nextCall[i]--;
+					break;
+				}
+			}
+		}
+		return nextCall;
+	}
+
+	private static int findFurthestIndex(int[] nextCall, int cSize) {
+		int furthestIndex = 0;
+		int furthestValue = nextCall[0];
+		for (int i = 1; i < cSize; i++) {
+			if (furthestValue < nextCall[i]) {
+				furthestValue = nextCall[i];
+				furthestIndex = i;
+			}
+		}
+		return furthestIndex;
 	}
 
 	// evict LFD
@@ -114,7 +171,26 @@ class COMP108A1Paging {
 	// rArray is an array containing the requeset sequence with rSize entries	
 	static COMP108A1Output evictLFD(int[] cArray, int cSize, int[] rArray, int rSize) {
 		COMP108A1Output output = new COMP108A1Output();
-
+		int indexToEvict = 0;
+		int[] nextCall = new int[cSize];
+		for (int i = 0; i < rSize; i++) {
+			boolean foundReq = false;
+			for (int j = 0; j < cSize; j++) {
+				if (cArray[j] == rArray[i]) {
+					foundReq = true;
+				}
+			}
+			if (!foundReq) {
+				output.missCount++;
+				output.hitPattern += "m";
+				nextCall = generateDistances(cArray, cSize, rArray, rSize);
+				indexToEvict = findFurthestIndex(nextCall, cSize);
+				cArray[indexToEvict] = rArray[i];
+			} else {
+				output.hitCount++;
+				output.hitPattern += "h";
+			}
+		}
 		return output;
 	}
 
