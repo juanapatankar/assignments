0a5ff8fdcbaf88f0cc8c892b86efba7323619ef9
Juana Patankar <juanapatankar4@gmail.com>
2021-03-28 16:07:46 +0100

    added comments

--- 124/Assignments/ass1.2/assignment1v2/assignment1v2/COMP124_Template.cpp
+++ 124/Assignments/ass1.2/assignment1v2/assignment1v2/COMP124_Template.cpp
@@ -5,40 +5,43 @@
 
 int main(void) {
 	// Declare variables here (C syntax)
+	// Declare prompt messages
 	char msg[] = "How many numbers: ";
 	char getInput[] = "Enter a number: ";
-	char zeromsg[] = "Number of 0s: ";
-	char posmsg[] = "Number of +ves: ";
-	char negmsg[] = "Number of -ves: ";
-	char newln[] = "\n";
-	char ifmt[] = "%i";
+
+	// Declare messages for each count output
+	char zeromsg[] = "\nNumber of 0s: ";
+	char posmsg[] = "\nNumber of +ves: ";
+	char negmsg[] = "\nNumber of -ves: ";
 	char fmt[] = "%d";
-	int remaining = 0;
+	
+	// Declare variables to store counts, and the current number
 	int zerocount = 0;
 	int poscount = 0;
 	int negcount = 0;
+	int remaining = 0;
 	int current = 0;
 
 	_asm {
 		// Put assembly code here
 		lea eax, msg
 		push eax
-		call printf
+		call printf					; load a prompt for the number of numbers that will be inputted
 		pop eax
 		lea ebx, remaining
 		push ebx
 		lea eax, fmt
 		push eax
-		call scanf
+		call scanf					; store the user's input as an integer, to the C variable remaining
 		pop eax
 		pop ebx
 		mov ebx, remaining
 		cmp ebx, 0
 		jg newnumber
-		jmp output
+		jmp output					; receive the first number, or jump straight to output
 
 	incpos:
-		add [poscount], 1
+		add[poscount], 1
 		jmp outornew
 
 	incneg:
@@ -50,88 +53,77 @@ int main(void) {
 		jmp outornew
 
 	newnumber:
+		sub ebx, 1					; decrement the number of inputs remaining
 		lea eax, getInput
 		push eax
-		call printf
+		call printf					; print a prompt for a number to be tested
 		pop eax
+
 		lea eax, current
 		push eax
 		lea eax, fmt
 		push eax
-		call scanf
+		call scanf					; store the inputted number to current
 		pop eax
 		pop eax
+
 		mov eax, current
-		sub ebx, 1				; restart loop to get next number, or print output
-		mov eax, ebx
-		cmp current, 0
-		jg incpos
+		
+		cmp current, 0				; test if the inputted number is positive, negative, or zero
+		jg incpos					;jumps to increment the approporiate count
 		jl incneg
 		je inczero
 
 	outornew:
-		cmp ebx, 0
-		jz output
-		jmp newnumber
+		cmp ebx, 0					; tests if there are more numbers to input
+		jz output					; output counts if no more numbers
+		jmp newnumber				; get the next input (loop)
 	
 	
 
 	
 
 	output: 
-		lea eax, zeromsg
+		lea eax, zeromsg			; print label(zero count)
 		push eax
 		call printf
 		pop eax
 
-
-		
 		mov eax, [zerocount]
 		push eax
 		lea eax, fmt
 		push eax
-		call printf
+		call printf					; print how many zeroes were inputted
 		pop eax
+		pop eax
+			
+		lea eax, posmsg				; print label(positive count)
+		push eax
+		call printf
 		pop eax
 
-		lea eax, newln
-		push eax
-		call printf
-		pop eax
-		lea eax, posmsg
-		push eax
-		call printf
-		pop eax
-		
 		mov eax, [poscount]
 		push eax
 		lea eax, fmt
 		push eax
-		call printf
+		call printf					; print how many numbers were positive
 		pop eax
 		pop eax
 
-		lea eax, newln
+		lea eax, negmsg				; print label, negative count
 		push eax
 		call printf
 		pop eax
-		lea eax, negmsg
-		push eax
-		call printf
-		pop eax
-		
+			
 		mov eax, [negcount]
 		push eax
 		lea eax, fmt
 		push eax
-		call printf
+		call printf					; print total number of negative integers
 		pop eax
 		pop eax
 
-		lea eax, newln
-		push eax
-		call printf
-		pop eax
+		
 	}
 return 0;
 }
