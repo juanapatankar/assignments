7419b08dbf646460299d1c14ce542e817aa302bd
Juana Patankar <juanapatankar4@gmail.com>
2021-04-02 18:56:01 +0100

    substitution.java compiles

--- Substitution.java
+++ Substitution.java
@@ -1,4 +1,4 @@
-class abstract Substitution implements Cipher {
+abstract class Substitution implements Cipher {
     public abstract char encrypt(char c);
     public abstract char decrypt(char c);
 
