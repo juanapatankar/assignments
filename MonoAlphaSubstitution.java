e9b5787bdd057062cc6e9505a471c87e0f51f14d
Juana Patankar <juanapatankar4@gmail.com>
2021-04-05 09:06:39 +0100

    implemented decrypt

--- MonoAlphaSubstitution.java
+++ MonoAlphaSubstitution.java
@@ -1,5 +1,6 @@
 public class MonoAlphaSubstitution extends Substitution {
     public char[] translate;
+    static String alphabet = "abcdefghijklmnopqrstuvwxyz";
     public MonoAlphaSubstitution() {
         translate = new char[] {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
     }
@@ -14,7 +15,6 @@ public class MonoAlphaSubstitution extends Substitution {
         
     }
     public static int letterToNumber(char toFind) {
-        String alphabet = "abcdefghijklmnopqrstuvwxyz";
         int pos = 0;
         for (int i = 0; i < alphabet.length(); i++) {
             if (alphabet.charAt(i) == toFind) {
@@ -24,6 +24,23 @@ public class MonoAlphaSubstitution extends Substitution {
         return pos;
     }
 
+    public static int findIndex(char toFind, char[] mapping) {
+        for (int i = 0; i < mapping.length; i++) {
+            if (mapping[i] == toFind) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static Boolean isLetter(char test) {
+        for (int i = 0; i < alphabet.length(); i++) {
+            if (alphabet.charAt(i) == test) {
+                return true;
+            }
+        }
+        return false;
+    }
     public char encrypt (char toChange) {
         int letter;
         Boolean cap = false;
@@ -33,8 +50,8 @@ public class MonoAlphaSubstitution extends Substitution {
             toChange = Character.toLowerCase(toChange);
         }
         letter = letterToNumber(toChange);
-        if (toChange == ' ') {
-            return ' ';
+        if (!isLetter(toChange)) {
+            return toChange;
         } else {
             if (!cap) {
                 return translate[letter];
@@ -46,7 +63,23 @@ public class MonoAlphaSubstitution extends Substitution {
     }
 
     public char decrypt(char toChange) {
-        return 'a';
+        Boolean cap = false;
+        String caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        char letter = 'a';
+        if (caps.contains(Character.toString(toChange))) {
+            cap = true;
+            toChange = Character.toLowerCase(toChange);
+        }
+        if (!isLetter(toChange)) {
+            return toChange;
+        } else {
+            letter = caps.charAt(findIndex(Character.toLowerCase(toChange), translate));
+        }
+        if (!cap) {
+            return Character.toLowerCase(letter);
+        } else {
+            return letter;
+        }
     }
     public static void main(String[] args) {
         MonoAlphaSubstitution tr;
@@ -62,6 +95,17 @@ public class MonoAlphaSubstitution extends Substitution {
                 }
                 System.out.println(done);
             } 
+        }
+        if (args[0].contains("decrypt")) {
+            if (args.length == 2) {
+                System.out.println(args[1]);
+            } else {
+                tr = new MonoAlphaSubstitution(args[1]);
+                for (int i = 0; i < args[2].length(); i++) {
+                    done += tr.decrypt(args[2].charAt(i));
+                }
+                System.out.println(done);
+            }
         }
     }
 }
