bb426973a095a101e69e35c402bcdc79a8a6a609
Juana Patankar <juanapatankar4@gmail.com>
2021-04-05 20:05:51 +0100

    fix position

--- Vigenere.java
+++ Vigenere.java
@@ -1,10 +1,13 @@
 class Vigenere extends Substitution {
     public char[][] translate;
+    public int position;
+    public String mapping;
     public static String alphabet = "abcdefghijklmnopqrstuvwxyz";
     public Vigenere() {
         translate = new char[][] {{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}};
     }
     public Vigenere(String mapping) {
+        this.mapping = mapping;
         translate = new char[mapping.length()][26];
         for (int i = 0; i < mapping.length(); i++) {
             int shift = letterToNumber(mapping.charAt(i));
@@ -17,6 +20,7 @@ class Vigenere extends Substitution {
                 translate[i][j] = newchar;
             }
         }
+        position = 0;
 
     }
     public static int letterToNumber(char toFind) {
@@ -28,21 +32,74 @@ class Vigenere extends Substitution {
         }
         return pos;
     }
+    public static Boolean isLetter(char test) {
+        for (int i = 0; i < alphabet.length(); i++) {
+            if (alphabet.charAt(i) == test) {
+                return true;
+            }
+        }
+        return false;
+    }
     public char encrypt(char toChange) {
-        return 'a';
+        String caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        Boolean cap = false;
+        if (caps.contains(Character.toString(toChange))) {
+            cap = true;
+            toChange = Character.toLowerCase(toChange);
+        }
+        if (isLetter(toChange)) {
+            int mappos = this.position;
+            if (mappos != 0) {
+                mappos = mapping.length() % position;
+            }
+            this.position++;
+            int letter = letterToNumber(toChange);
+            System.out.println(mappos);
+            toChange = translate[mappos][letter];
+            // switch between caps/lowercase:
+            if (!cap) {
+                return toChange;
+            } else {
+                return Character.toUpperCase(toChange);
+            }
+        } else {
+            return toChange;
+        }
     }
     public char decrypt(char toChange) {
         return 'a';
     }
     public static void main(String[] args) {
         Vigenere tr;
+        String done = "";
+
+        if (args.length < 3) {
+            System.out.println("Too few parameters!\nUsage: java Vigenere encrypt key \"cipher text\"");
+        } else if (args.length > 3) {
+            System.out.println("Too many parameters!\nUsage: java Vigenere encrypt key \"cipher text\"");
+        } else if (!(args[0].contains("encrypt") || args[0].contains("decrypt"))) {
+            System.out.println("The first parameter must be \"encrypt\" or \"decrypt\"!\nUsage: java Vigenere encrypt key \"cipher text\"");
+        } else {
+            if (args[0].contains("encrypt")) {
+                tr = new Vigenere(args[1].toLowerCase());
+                for (int i = 0; i < args[2].length(); i++) {
+                    done += tr.encrypt(args[2].charAt(i));
+                }
+                System.out.println(done);
+            }
+        }
+
+        
+        /* 
         if (args.length == 1) {
             tr = new Vigenere(args[0]);
-        } else {
+            for (int i = 0; i < args[0].length(); i++) {
+                done += tr.encrypt(args[0].charAt(i));
+            }
+            System.out.println(done);
+        } else if (args.length == 0) {
             tr = new Vigenere();
-        }
-        for (int i = 0; i < args[0].length(); i++) {
-            System.out.println(String.valueOf(tr.translate[i])+"\n");
-        }
+        } */
+        
     }
 }
