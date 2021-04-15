dd9e1fd0794726cf91a3bbc5dd2e8eddc95b115f
Juana Patankar <juanapatankar4@gmail.com>
2021-04-15 15:00:16 +0100

    fixed assignment 1

---
+++ 124/Labs/Lab2/Lab2/COMP124_Template.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	// Declare variables here (C syntax)
+	int count1 = -10;
+	int count2 = 5;
+	int num = 0;
+	_asm {
+		// Put assembly code here
+		
+	}
+	return 0;
+}
